4572150c9ac809e5b4ceab258d0f1a5f094f732b
Shanelle Margaret Navarro <156684143+sheeshabolshaning@users.noreply.github.com>
2025-04-24 20:39:21 +0800

    --added a back button & added minor revisions

--- app/src/main/java/com/example/dormspot/OnLaunch/RegisterActivity.java
+++ app/src/main/java/com/example/dormspot/OnLaunch/RegisterActivity.java
@@ -1,4 +1,17 @@
 package com.example.dormspot.OnLaunch;
 
-public class RegisterActivity {
-}
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.dormspot.R;
+
+    public class RegisterActivity extends AppCompatActivity {
+        protected void onCreate(Bundle savedInstanceState) {
+
+
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_register);
+
+        }
+    }
