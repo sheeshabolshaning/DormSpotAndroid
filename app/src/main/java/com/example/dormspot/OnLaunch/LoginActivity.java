323419d3b45f24a5a4a4e883149f95ea0e2af648
Shanelle Margaret Navarro <156684143+sheeshabolshaning@users.noreply.github.com>
2025-04-27 20:17:21 +0800

    -- modified the register activity and addded its functionality (Still need to figure out why the fields arent showing)

--- app/src/main/java/com/example/dormspot/OnLaunch/LoginActivity.java
+++ app/src/main/java/com/example/dormspot/OnLaunch/LoginActivity.java
@@ -1,6 +1,10 @@
 package com.example.dormspot.OnLaunch;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.dormspot.R;
@@ -10,6 +14,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
 
+        // TextView listener for "Don't have an account?"
+        TextView textView = findViewById(R.id.textDontHaveAccount);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to RegisterActivity
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+    }
 }
--- app/src/main/java/com/example/dormspot/OnLaunch/RegisterActivity.java
+++ app/src/main/java/com/example/dormspot/OnLaunch/RegisterActivity.java
@@ -1,6 +1,7 @@
 package com.example.dormspot.OnLaunch;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +13,8 @@ import com.example.dormspot.R;
 
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_register);
+
+            Toast.makeText(this, "Register Activity Opened!", Toast.LENGTH_SHORT).show();
 
         }
     }
