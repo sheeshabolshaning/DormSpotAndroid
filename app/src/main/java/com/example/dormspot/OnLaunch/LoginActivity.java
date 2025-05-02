577dbc59f709c98526e02f6812d37c2f2f67b7d0
Shanelle Margaret Navarro <156684143+sheeshabolshaning@users.noreply.github.com>
2025-05-02 15:57:04 +0800

    added back button functionality

--- app/src/main/java/com/example/dormspot/OnLaunch/LoginActivity.java
+++ app/src/main/java/com/example/dormspot/OnLaunch/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.dormspot.OnLaunch;
 
+import android.widget.ImageButton;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -14,6 +15,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Handle back button click
+        ImageButton backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish(); // Close LoginActivity and go back to the previous screen
+            }
+        });
 
         // TextView listener for "Don't have an account?"
         TextView textView = findViewById(R.id.textDontHaveAccount);
