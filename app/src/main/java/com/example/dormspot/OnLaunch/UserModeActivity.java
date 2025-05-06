a31c6fbf88738d7e365a50797aca2d2bffeec23d
Shanelle Margaret Navarro <156684143+sheeshabolshaning@users.noreply.github.com>
2025-05-06 16:11:03 +0800

    Added login function

--- app/src/main/java/com/example/dormspot/OnLaunch/UserModeActivity.java
+++ app/src/main/java/com/example/dormspot/OnLaunch/UserModeActivity.java
@@ -1,4 +1,22 @@
 package com.example.dormspot.OnLaunch;
 
-public class UserModeActivity {
+import android.os.Bundle;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.dormspot.R;
+
+public class UserModeActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_mode);
+    }
 }
--- app/src/main/java/com/example/dormspot/OnLaunch/WelcomeActivity.java
+++ app/src/main/java/com/example/dormspot/OnLaunch/WelcomeActivity.java
@@ -1,22 +1,27 @@
 package com.example.dormspot.OnLaunch;
 
-import android.os.Bundle;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.TextView;
-import android.widget.Toast;
-
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.dormspot.R;
 
-public class WelcomeActivity extends AppCompatActivity{
+public class WelcomeActivity extends AppCompatActivity {
+
+    private Button btnGetStarted;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        btnGetStarted = findViewById(R.id.btnGetStarted);
+
+        btnGetStarted.setOnClickListener(v -> {
+            Intent intent = new Intent(WelcomeActivity.this, UserModeActivity.class);
+            startActivity(intent);
+        });
     }
 }
